1164d871c321e2997d70dd9180ebd74c961a9790
cBeal <169796@supinfo.com>
2014-09-13 20:36:42 +0200

    Added time duration and true average speed thanks to distance/time

--- main.cpp
+++ main.cpp
@@ -22,15 +22,44 @@ double getDistance(double lat1,double lon1,double lat2,double lon2) {
     double a = (sin(dLat/2) * sin(dLat/2)) + cos(lat1) * cos(lat2) * sin(dLon/2) * sin(dLon/2);
     double c = 2 * atan2(sqrt(a), sqrt(1-a));
     double d = R * c;
+    return d;
+}
 
-   return d;
+double getSeconds(string date){
+    unsigned int i;
+    int hour, minute, second, field;
+    string buffer;
+
+    field = 0;
+    for(i=0;i<date.size();i++){
+        if(date[i] == ':'){
+            switch(field){
+                case 0:
+                    hour = atoi(buffer.c_str());
+                    break;
+                case 1:
+                    minute = atoi(buffer.c_str());
+                    break;
+                case 2:
+                    second = atoi(buffer.c_str());
+                    break;
+            }
+            field++;
+            buffer.clear();
+        }else{
+            buffer += date[i];
+        }
+    }
+    return hour*3600 + minute*60 + second;
+
+
 }
 
 int main(int argc, char* argv[])
 {
-    unsigned int index;
+    unsigned int index, total_time, end_idx;
     string arguments, first, data, final_data, speed;
-    double speed_dbl, total_speed, avg_speed, slope_up, slope_down, delta_elevation;
+    double speed_dbl, total_speed, avg_speed, slope_up, slope_down, delta_elevation, total_crow_fly;
     bool is_write = false,
             is_output = false,
             is_latitude = true;
@@ -39,6 +68,7 @@ int main(int argc, char* argv[])
     vector<double> latitude;
     vector<double> longitude;
     vector<double> elevation;
+    vector<string> hour;
     vector<double> speed_list;
     vector<double> speed_list_avg;
 
@@ -70,8 +100,12 @@ int main(int argc, char* argv[])
     }
 
     string path = "\"" + arguments + "\"";
-
     ifstream fichier(arguments.c_str(), ios::in);
+
+    ///Debug
+    //string file_name = "debug";
+    //ifstream fichier("C:\\GPS-.gpx-decoder\\bin\\Debug\\2014-09-07_15-50-01.gpx", ios::in);
+
 
     if(fichier){
         string contenu;
@@ -93,7 +127,7 @@ int main(int argc, char* argv[])
                                 longitude.push_back(atof(data.c_str()));
                                 is_latitude = true;
                             }
-                            data.clear();
+                            data = "";
                         }else{
                             save_data = true;
                         }
@@ -113,6 +147,22 @@ int main(int argc, char* argv[])
                             }else if(contenu[index] == '<' && save_data){
                                 save_data = false;
                                 elevation.push_back(atof(data.c_str()));
+                            }else{
+                                if (save_data){
+                                    data += contenu[index];
+                                }
+                            }
+                        }
+                    }
+
+                    if(contenu.find("<tim") != string::npos){
+                        data = "";
+                        for(index = 0; index < contenu.size(); index++){
+                            if(contenu[index] == 'T'){
+                                save_data = true;
+                            }else if(contenu[index] == 'Z' && save_data){
+                                save_data = false;
+                                hour.push_back(data);
                             }else{
                                 if (save_data){
                                     data += contenu[index];
@@ -144,9 +194,8 @@ int main(int argc, char* argv[])
                     }
                     getline(fichier, contenu);
                     save_data = false;
-                    data.clear();
+                    data = "";
                 }while (contenu.find("</trkpt") == string::npos);
-
             }
         }
 
@@ -157,9 +206,31 @@ int main(int argc, char* argv[])
 
     fichier.close();
 
+    for(index = 0 ; index < speed_list.size() ; index++){
+
+        if(speed_list[index] > 0.001){
+            speed_list_avg.push_back(speed_list[index]);
+            total_speed += speed_list[index];
+        }
+
+        if(index>1){
+            delta_elevation = elevation[index] - elevation[index-1];
+            if(delta_elevation > 0){
+                slope_up += delta_elevation;
+            }else{
+                slope_down += delta_elevation;
+            }
+        }
+    }
+    end_idx = hour.size() - 1;
+    total_time = getSeconds(hour[end_idx]) - getSeconds(hour[0]);
+    avg_speed = total_speed / (speed_list_avg.size() - 1);
+    total_crow_fly = getDistance(latitude[0], longitude[0], latitude[latitude.size()-1], longitude[longitude.size()-1] );
+
     if(is_write){
        ofstream csv_output(file_name.c_str(), ofstream::out | ofstream::app);
         if(csv_output.is_open()){
+            csv_output << "Latitude,Longitude,Altitude,Speed,Distance" << endl;
             for (index = 0; index < latitude.size(); index++){
                 csv_output << latitude[index] << "," << longitude[index] << "," << elevation[index] << "," << speed_list[index];
                 if (index+1 > latitude.size()){
@@ -168,6 +239,12 @@ int main(int argc, char* argv[])
                     csv_output << "," << getDistance(latitude[index], longitude[index], latitude[index+1], longitude[index+1]) << endl;
                 }
             }
+            csv_output << "Total crow-fly distance(km)," << total_crow_fly << endl;
+            csv_output << "Average speed GPS (km/h)," << avg_speed << endl;
+            csv_output << "Average speed Time/distance (km/h)," << 1.0*total_crow_fly/(1.0*total_time/3600) << endl;
+            csv_output << "Slope up(m)," << slope_up << endl;
+            csv_output << "Slope down(m)," << -slope_down << endl;
+            csv_output << "Total time(s)," << total_time << endl;
             csv_output.close();
 
         }else{
@@ -176,29 +253,10 @@ int main(int argc, char* argv[])
     }
 
     if(is_output){
-        cout << "La distance entre le depart et l'arrivee est : " << getDistance(latitude[0], longitude[0], latitude[latitude.size()-1], longitude[longitude.size()-1] ) << endl;
+        cout << "La distance entre le depart et l'arrivee est : " << total_crow_fly << " ,le temps est de : "<< total_time << endl;
         cout << file_name << endl;
-
-        for(index=0 ; index < speed_list.size() ; index++){
-            if(speed_list[index] > 0.001){
-                speed_list_avg.push_back(speed_list[index]);
-                total_speed += speed_list[index];
-            }
-            if(index>1){
-                delta_elevation = elevation[index] - elevation[index-1];
-                if(delta_elevation > 0){
-                    slope_down += delta_elevation;
-                }else{
-                    slope_up += delta_elevation;
-                }
-
-            }
-
-        }
-
-        avg_speed = total_speed / speed_list_avg.size();
-        cout << "La vitesse moyenne est de :" << avg_speed << endl;
-        cout << "Le denivele est de +" << -slope_up << " metres et de -" << slope_down <<" metres";
+        cout << "La vitesse moyenne est de :" << avg_speed << "ou selon le ration distance/temps :" << 1.0*total_crow_fly/(1.0*total_time/3600)<< endl;
+        cout << "Le denivele est de +" << slope_up << " metres et de -" << -slope_down <<" metres";
     }
     return 0;
 }
